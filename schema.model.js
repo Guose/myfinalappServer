824fa3f06d181d89691db96118694ead31f21091
Justin Elder <guose79@gmail.com>
2023-09-21 17:57:45 -0700

    Changes made to schema and added PUT method to update MongoDB

--- schema.model.js
+++ schema.model.js
@@ -1,19 +1,27 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-let NewSchema = new Schema({
+let TodoSchema = new Schema({
     description: {
-        type: String
+        type: String,
+        required: true
     },
     priority: {
-        type: String
+        type: String,
+        required: true
+    },
+    createdOn: {
+        type: Date,
+        default: Date.now(),
+        required: true,
     },
     completed: {
-        type: String
+        type: Date,
+        required: true
     },
     done: {
-        type: Boolean
+        type: Boolean,
     }
 })
 
-module.exports = mongoose.model('Schema', NewSchema)
+module.exports = mongoose.model('Todo', TodoSchema)
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const app = express()
-let ToDoSchema = require('./schema.model')
+const TodoSchema = require('./schema.model')
 const PORT = process.env.PORT || 3001
 
 app.use(cors())
@@ -11,28 +11,60 @@ app.use(bodyParser.json())
 
 mongoose.connect('mongodb+srv://justinelder:Mypass123@project1.vxblm.mongodb.net/FinalDB?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true })
 const connection = mongoose.connection
-connection.once('open', function() {
+connection.once('open', () => {
     console.log('DATABASE IS CONNECTED')
 })
 
-app.delete('/delete', function(req, res) {
+app.delete('/delete', (req, res) => {
     console.log('DELETED: ', req.body)
     let id = req.body.id
-    ToDoSchema.findByIdAndDelete(id, function(err, deletion) {
+    TodoSchema.findByIdAndDelete(id, (err, deletion) => {
         res.send(deletion)
     })
 })
 
-app.get('/list', function(req, res) {
-    ToDoSchema.find(function (err, response) {
+app.get('/list', (req, res) => {
+    TodoSchema.find( (err, response) => {
         res.json(response)
     })
+}).patch((err) => {
+    console.log(err.message)
 })
 
-app.post('/create', function(req, res) {
+app.put('/update', (req, res) => {
+    try {
+        let id = req.body.data._id
+        console.log('id to update: ', id)
+        if (!id) {
+            return res.status(400).json({ error: 'ID is not found' })
+        }
+
+        // Add the update data as the second argument
+        TodoSchema.findByIdAndUpdate(id, req.body.data, { new: true }, (err, updatedToDo) => {
+            if (err) {
+                console.error('Error updating ToDo:', err)
+                return res.status(500).json({ error: 'An error occurred while updating.' })
+            }
+
+            if (!updatedToDo) {
+                return res.status(404).json({ error: 'ToDo not found.' })
+            }
+
+            // Send response here
+            console.log('response sent: ', updatedToDo)
+            res.json(updatedToDo)
+        });
+    } catch (error) {
+        console.error('try/catch error: ', error.message)
+        res.status(400).json({ error: 'An error occurred - PUT try/catch' })
+    }
+})
+
+
+app.post('/create', (req, res) => {
     console.log('REQ.BODY: ', req.body)
     let newTodo = req.body
-    let todo = new ToDoSchema(newTodo)
+    let todo = new TodoSchema(newTodo)
     console.log('TODO: ', todo)
     todo.save()
     .then(todo => {
@@ -40,6 +72,6 @@ app.post('/create', function(req, res) {
     })
 })
 
-app.listen(PORT, function(req, res) {
-    console.log('Listening on Port 3001')
+app.listen(PORT, () => {
+    console.log('Listening on Port ', PORT)
 })
